8ff1496ee4ed70d3aa300e1aefd121375792bc15
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-23 02:13:44 -0500

    auth endpoints created, working on db models

---
+++ auth/authRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router(); 
+
+// security 
+const bcrypt = require('bcryptjs'); 
+
+// login
+router.post('/login', (req, res) => {
+
+})
+
+// register
+router.post('/register', (req, res) => {
+    
+})
