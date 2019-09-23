71b069f1c6dae54f5fc8dc2daeb60abd36aaf5eb
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-23 01:26:15 -0500

    project structure setup, moving to "elijah-mckay" branch

---
+++ api/server.js
@@ -0,0 +1,24 @@
+// library imports
+const express = require('express'); 
+const helmet = require('helmet'); 
+const cors = require('cors');
+
+//server creation
+const server = express();  
+
+// middleware instantiation
+server.use(express.json()); 
+server.use(cors()); 
+server.use(helmet()); 
+
+// router imports 
+const authRouter = require('../auth/authRouter'); 
+const valueRouter = require('../values/valueRouter'); 
+const taskRouter = require('../tasks/task-model');
+
+// routing
+server.use('/auth/', authRouter); 
+server.use('/api/values', valueRouter); 
+server.use('/api/tasks', taskRouter); 
+
+module.exports = server; 
